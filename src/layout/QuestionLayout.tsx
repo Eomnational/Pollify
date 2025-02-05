99baf420e614d4c21812795223f7ff1ead038c36
xunana <3520653564@qq.com>
2025-02-05 15:42:13 +0800

    feat: 实现layout模版

---
+++ src/layout/QuestionLayout.tsx
@@ -0,0 +1,21 @@
+import React,{FC} from'react';
+
+const QuestionLayout:FC = () => {
+  return (
+        <>
+        <div>
+        QuestionLayout header
+        </div>
+        <div>
+        QuestionLayout container
+        </div>
+        <div>
+        QuestionLayout footer
+        </div>
+        </>
+  )
+}
+
+
+
+  export default QuestionLayout;
